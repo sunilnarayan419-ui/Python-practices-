2c6abe04737221fae559e176122472227b69d1b0
[SUNIL MANDLOI] <[sunilnarayan419@gmail.com]>
2026-01-16 22:19:14 +0530

    This is done

--- 00_Python Data Type/00_String DataType.py
+++
@@ -1,8 +0,0 @@
-# String Data Type 
-# immutable sequence data type 
-
-a_str = "Sunil Narayan"
-
-print(a_str)
-print(a_str[0]) 
-print(a_str[0:3]) 
--- 00_Python Data Type/02_Numbers data type.py
+++
@@ -1,7 +0,0 @@
-# Numbers data type 
-# 3 type  
-int_num = 10 # int value
-float_num = 10.23 # float type 
-complex_num = 3.14j # complex type 
-
-
